7e3c2812190bf76a01367cd75b9c91e4270b92c2
Vidhu Tanwar <tanwarvu@mail.uc.edu>
2023-04-18 11:58:32 -0400

    Authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "iNotebookisverygood";
 
 router.get('/', (req, res) => {
     res.json({ success: true, message: "Welcome to iNotebook API" })
@@ -20,13 +24,20 @@ router.post('/', [
             return res.status(400).json({ errors: errors.array() });
         }
 
+        const salt = await bcrypt.genSaltSync(10);
+        const hash = await bcrypt.hashSync(req.body.password, salt);
+
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ success: false, message: "Sorry a user with this email already exists" })
 
         }
-         user = await User.create(req.body);
-        res.json({ success: true, message: "Successfully created a new user", user: user })
+        user = await User.create({ name: req.body.name, email: req.body.email, password: hash });
+        const data = { user: { id: user.id } };
+
+        const token = jwt.sign(data, JWT_SECRET);
+
+        res.json({token});
     } catch (error) {
         res.status(500).json({ success: false, message: "Internal Server Error", error: error.message })
     }
